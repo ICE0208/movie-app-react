432b6780960264397582a72d0e5cc2ad8d3cad32
ICE0208 <doky3307@naver.com>
2023-10-03 18:58:58 +0900

    NowPlaying 페이지 > 영화 목록 불러오기

--- src/routers/nowplaying/index.tsx
+++ src/routers/nowplaying/index.tsx
@@ -1,4 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { Wrapper } from '../../styles';
+import { IAPIResponse, getNowPlaying } from '../../api';
+import { MovieList } from '../../components';
+
 function NowPlaying() {
-  return <div>NowPlaying</div>;
+  const { data, isLoading } = useQuery<IAPIResponse>({
+    queryKey: ['movies', 'nowplaying'],
+    queryFn: getNowPlaying,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <span>Loading</span>
+      ) : (
+        <MovieList movies={data?.results || []} />
+      )}
+    </Wrapper>
+  );
 }
+
 export default NowPlaying;
