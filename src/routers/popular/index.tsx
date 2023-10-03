0245c9238f163a22d241728ecb6b6564436193dc
ICE0208 <doky3307@naver.com>
2023-10-03 18:15:48 +0900

    Popular Movie API 호출 테스트

--- src/routers/popular/index.tsx
+++ src/routers/popular/index.tsx
@@ -1,4 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import { Wrapper } from '../../styles';
+import { IAPIResponse, getPopular } from '../../api';
+
 function Popular() {
-  return <div>Popular</div>;
+  const { data, isLoading } = useQuery<IAPIResponse>({
+    queryKey: ['movies', 'popular'],
+    queryFn: getPopular,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(data);
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <span>Loading</span>
+      ) : (
+        <ul>
+          {data?.results.map((movie) => <li key={movie.id}>{movie.title}</li>)}
+        </ul>
+      )}
+    </Wrapper>
+  );
 }
 export default Popular;
