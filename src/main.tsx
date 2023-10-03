46b4b48b9a62e018adcd31a244a08ce050df76f0
ICE0208 <doky3307@naver.com>
2023-10-03 18:09:31 +0900

    GlobalStyle.ts 경로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import Globalstyle from './GlobalStyle.ts';
+import Globalstyle from './styles/GlobalStyle.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
