3c9de28ab7b746f536139bdf1a7cbd0922c67e81
ICE0208 <doky3307@naver.com>
2023-10-03 18:07:50 +0900

    Wrapper 스타일 컴포넌트 추가

---
+++ src/styles/Wrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  background-color: black;
+  color: white;
+  padding: 50px;
+`;
+
+export default Wrapper;
