7679a90ba04da2afb3398ba18d64fda70cd3078a
ICE0208 <doky3307@naver.com>
2023-10-03 18:55:06 +0900

    리팩토링 : MovieItem 컴포넌트 추가 / 분리

---
+++ src/components/MovieItem.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { IMovie, makeImagePath } from '../api';
+
+function MovieItem({ movie }: { movie: IMovie }) {
+  return (
+    <Item key={movie.id}>
+      <Img src={makeImagePath(movie.poster_path)} />
+      <Title>{movie.title}</Title>
+    </Item>
+  );
+}
+const Item = styled.div`
+  border-radius: 20px;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 450px;
+  object-fit: cover;
+  border-radius: 20px;
+`;
+
+const Title = styled.div`
+  padding: 20px 4px;
+  font-size: 26px;
+  text-align: center;
+  font-weight: bold;
+`;
+export default MovieItem;
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
-import { IMovie, makeImagePath } from '../api';
+import { IMovie } from '../api';
+import { MovieItem } from '.';
 
 function MovieList({ movies }: { movies: IMovie[] }) {
   return (
     <Container>
-      {movies.map((item) => (
-        <Item key={item.id}>
-          <Img src={makeImagePath(item.poster_path)} />
-          <Title>{item.title}</Title>
-        </Item>
+      {movies.map((movie) => (
+        <MovieItem movie={movie} />
       ))}
     </Container>
   );
@@ -20,24 +18,6 @@ const Container = styled.div`
   grid-gap: 44px 30px;
   width: 100%;
   justify-content: center;
-`;
-
-const Item = styled.div`
-  border-radius: 20px;
-`;
-
-const Img = styled.img`
-  width: 100%;
-  height: 450px;
-  object-fit: cover;
-  border-radius: 20px;
-`;
-
-const Title = styled.div`
-  padding: 20px 4px;
-  font-size: 26px;
-  text-align: center;
-  font-weight: bold;
 `;
 
 export default MovieList;
